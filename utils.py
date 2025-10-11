74a20830a79ad223c08827046aa073fec296ddd8
kirrrakirrra <kirrra419904@gmail.com>
2025-10-11 13:29:42 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
-# utils.py
 from datetime import datetime
 import pytz
 
-# Возвращает текущее время во Вьетнаме (Asia/Ho_Chi_Minh, GMT+7)
-def get_now_local():
-    return datetime.now(pytz.timezone("Asia/Ho_Chi_Minh"))
+LOCAL_TZ = pytz.timezone("Asia/Ho_Chi_Minh")
+
+def now_local():
+    return datetime.now(LOCAL_TZ)
+
+def format_now():
+    return now_local().strftime("%Y-%m-%d %H:%M:%S")
